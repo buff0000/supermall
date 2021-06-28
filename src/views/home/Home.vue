701f2c940cda6d3638500d66008666cad51ceaf9
buff <wangshengjun@linksfield.net>
2021-06-29 00:19:49 +0800

    轮播图实现

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,41 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>购物街</template>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar.vue";
+import HomeSwiper from "views/home/childComps/HomeSwiper.vue";
+
+import { getHomeMultidata } from "network/home";
 export default {
   name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
 };
 </script>
 
 <style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <swiper>
+    <swiper-item v-for="item in banners" :key="item">
+      <a :href="item.link">
+        <div class="haha">
+          <img style="width: 100%"  :src="item.image" alt="" />
+        </div>
+      </a>
+    </swiper-item>
+  </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper";
+export default {
+  name: "HomeSwiper",
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+  props: {
+    //TODO: banners: Array  这种写法不对，
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* TODO: 此处写这个不管用，需要在img标签中写上才管用 
+.img {
+  width: 100%;
+} 
+*/
+</style>
